58955301cce82917f7c96e84b731f22c7e0b5433
leaf corcoran <leafot@gmail.com>
2024-06-25 13:56:21 -0700

    refine the fix transcription prompt

--- openai.go
+++ openai.go
@@ -81,7 +81,7 @@ func fixTranscription(ctx context.Context, transcribedText string, instructions 
 	var messages = []openai.ChatCompletionMessage{
 		{
 			Role:    "system",
-			Content: "You are an voice-to-text typing program that takes the textual result of an automated transcription and a context from the user's screen and fixes the transcription to be what the user likely intended to type. You will output only the updated transcription and no other text.",
+			Content: "You are an voice-to-text typing program that takes the textual result of an automated transcription and a context from the user's screen and fixes the transcription to be what the user likely intended to type. You will output only the updated transcription and no other text. Do not output information not said in the original transcription.",
 		},
 		{
 			Role:    "user",
