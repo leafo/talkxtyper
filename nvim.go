b780006552353ecbf5c663ac0a383047c21b65a1
leaf corcoran <leafot@gmail.com>
2024-06-25 13:08:31 -0700

    better function for extracting current context on the screen

--- nvim.go
+++ nvim.go
@@ -6,7 +6,44 @@ import (
 	"os/exec"
 	"regexp"
 	"strings"
+	"text/template"
 )
+
+var getVisibleTextCmd = template.Must(template.New("getVisibleTextCmd").Parse(`
+  local num_lines = {{.NumLines}}
+  local sigil = "{{.Sigil}}"
+
+  local current_line = vim.api.nvim_win_get_cursor(0)[1]
+  local total_lines = vim.api.nvim_buf_line_count(0)
+
+  local start_line = math.max(1, current_line - num_lines)
+  local end_line = math.min(total_lines, current_line + num_lines)
+
+  local before = vim.api.nvim_buf_get_lines(0, start_line-1, current_line - 1, false)
+  local after = vim.api.nvim_buf_get_lines(0, current_line, end_line, false)
+
+  -- get the current line in two parts split by the cursor position
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)[2]
+  local current_line_text = vim.api.nvim_buf_get_lines(0, current_line-1, current_line, false)[1]
+
+  local before_cursor = string.sub(current_line_text, 1, cursor_pos)
+  local after_cursor = string.sub(current_line_text, cursor_pos + 1)
+
+  -- join the lines with the cursor inserted in the middle
+  local lines = {}
+
+  for i, line in ipairs(before) do
+    table.insert(lines, line)
+  end
+
+  table.insert(lines, before_cursor .. sigil .. after_cursor)
+
+  for i, line in ipairs(after) do
+    table.insert(lines, line)
+  end
+
+  return table.concat(lines, "\n")
+`))
 
 type NvimClient struct {
 	socketFile string
@@ -117,10 +154,23 @@ func (client *NvimClient) RemoteExecute(command string) (string, error) {
 	return cleanOutput, nil
 }
 
-func (client *NvimClient) GetVisibleText() (string, error) {
-	visibleTextCmd := `join(getline('w0', line('.') - 1), "\n") . "{{CURSOR}}" . join(getline(line('.') + 1, 'w$'), "\n")`
+func (client *NvimClient) RemoteExecuteLua(command string) (string, error) {
+	command = strings.ReplaceAll(command, "'", "''")
+	luaCommand := fmt.Sprintf("luaeval('(function() %s end)()')", command)
+	return client.RemoteExecute(luaCommand)
+}
 
-	visibleTextOutput, err := client.RemoteExecute(visibleTextCmd)
+func (client *NvimClient) GetVisibleText(cursorSigil string) (string, error) {
+	var visibleTextCmd strings.Builder
+	err := getVisibleTextCmd.Execute(&visibleTextCmd, map[string]interface{}{
+		"NumLines": 20,
+		"Sigil":    cursorSigil,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	visibleTextOutput, err := client.RemoteExecuteLua(visibleTextCmd.String())
 	if err != nil {
 		return "", err
 	}
